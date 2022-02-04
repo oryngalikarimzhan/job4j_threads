7f2de6eb2fbe670c5fff020f23efdbcecadb1eeb
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-02-04 12:34:38 +0600

    1.1. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -13,7 +13,6 @@ public class ThreadState {
         first.start();
         second.start();
 
-
         while (first.getState() != Thread.State.TERMINATED
                 || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState());
