4fdb5f6f3ac552288d4aa3c45cd9fe8ae0f1a9a1
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-02-10 09:21:48 +0600

    4. ThreadSafe динамический список [#1105 #262558]

--- src/main/java/ru/job4j/synch/SingleLockList.java
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -4,7 +4,6 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,12 +13,11 @@ public class SingleLockList<T> implements Iterable<T> {
     private final List<T> list;
 
     public SingleLockList(List<T> list) {
-        this.list = Collections.synchronizedList(list);
+        this.list = copy(list);
     }
 
     public synchronized void add(T value) {
-        T copyNewValue = value;
-        list.add(copyNewValue);
+        list.add(value);
     }
 
     public synchronized T get(int index) {
@@ -27,12 +25,12 @@ public class SingleLockList<T> implements Iterable<T> {
         return copyGetValue;
     }
 
-    private synchronized List<T> copy() {
+    private synchronized List<T> copy(List<T> list) {
         return new ArrayList<>(list);
     }
 
     @Override
     public synchronized Iterator<T> iterator() {
-        return copy().iterator();
+        return copy(this.list).iterator();
     }
 }
