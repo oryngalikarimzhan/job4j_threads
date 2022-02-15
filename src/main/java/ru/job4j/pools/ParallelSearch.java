e938e4de8052422fb04bbe2f0afdeff51c4aeebb
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-02-15 13:06:14 +0600

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pools/ParallelSearch.java
@@ -0,0 +1,54 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelSearch<T> extends RecursiveTask<Integer> {
+    private final T[] array;
+    private final T object;
+    private final int from;
+    private final int to;
+
+    public ParallelSearch(T[] array, T object, int from, int to) {
+        this.array = array;
+        this.object = object;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (to - from <= 10) {
+            return searchIndex();
+        }
+        int mid = (from + to) / 2;
+        ParallelSearch<T> leftParallelSearcher = new ParallelSearch(array, object, from, mid);
+        ParallelSearch<T> rightParallelSearcher = new ParallelSearch(array, object, mid + 1, to);
+        leftParallelSearcher.fork();
+        rightParallelSearcher.fork();
+        Integer leftJoin = leftParallelSearcher.join();
+        Integer rightJoin = rightParallelSearcher.join();
+        return leftJoin > rightJoin ? leftJoin : rightJoin;
+    }
+
+    private int searchIndex() {
+        int rsl = -1;
+        for (int i = from; i <= to; i++) {
+            if (array[i].equals(object)) {
+                rsl = i;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Character[] array = new Character[127];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (char) i;
+        }
+        char object = '!';
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        int result = forkJoinPool.invoke(new ParallelSearch<>(array, object, 0, array.length - 1));
+        System.out.println(result);
+    }
+}
