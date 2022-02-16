6accaa7f7c581a8c4a7cdb4b193ac835929dfc05
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-02-17 01:02:23 +0600

    4. CompletableFuture [#361626 #265260]

--- src/main/java/ru/job4j/future/RolColSum.java
+++ src/main/java/ru/job4j/future/RolColSum.java
@@ -3,6 +3,7 @@ package ru.job4j.future;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -22,6 +23,23 @@ public class RolColSum {
                     + "rowSum=" + rowSum
                     + ", colSum=" + colSum
                     + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Sums sums = (Sums) o;
+            return rowSum == sums.rowSum && colSum == sums.colSum;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(rowSum, colSum);
         }
     }
 
